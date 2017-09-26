ed2d73420f579be2dfbfa79693d58b6a2ad1c734
alanlc <alluchan@ucsc.edu>
2017-09-25 23:38:32 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+//Alan Chan
+//Clock and Hourglass in P5.js
+//Inspired by Daniel ShiffmansCoding Train
+
 function setup() {
    createCanvas(800, 400);
    angleMode(DEGREES);
@@ -6,12 +10,23 @@ function setup() {
  function draw() {
    background(0);
    translate(200, 200);
-   rotate(-90);
-
+     
+     
    let hr = hour();
    let mn = minute();
    let sc = second();
    var MilSec = millis();
+
+   fill(255,255,255);
+   noStroke();
+   textSize(22);
+   text((hr%12) + ':' + mn + ':' + sc, -42, -175);
+   text((hr%12), 288, 0);
+   text(mn, 388, 0);
+   text(sc, 488, 0);
+
+
+   rotate(-90);
 
      
    //SECONDS
@@ -22,8 +37,8 @@ function setup() {
    arc(0, 0, 300, 300, 0, secondAngle);
    strokeWeight(2);
    fill(255,100,150);
-   arc(-100, 300, 88, 88, -1*(180 + (secondAngle/2)), (180 + (secondAngle/2)), CHORD);
-   arc(100, 300, 88, 88,((secondAngle/2))%180, (-1*(secondAngle/2))%180, CHORD);
+   arc(-77, 500, 88, 88, -1*(180 + (secondAngle/2)), (180 + (secondAngle/2)), CHORD);
+   arc(77, 500, 88, 88,((secondAngle/2))%180, (-1*(secondAngle/2))%180, CHORD);
 
    //MINUTES
    strokeWeight(8);
@@ -33,8 +48,8 @@ function setup() {
    arc(0, 0, 280, 280, 0, minuteAngle);
    fill(150,100,255);
    strokeWeight(2);
-   arc(-100, 400, 88, 88, -1*(180 + (minuteAngle/2)), (180 + (minuteAngle/2)), CHORD);
-   arc(100, 400, 88, 88,((minuteAngle/2))%180, (-1*(minuteAngle/2))%180, CHORD);
+   arc(-77, 400, 88, 88, -1*(180 + (minuteAngle/2)), (180 + (minuteAngle/2)), CHORD);
+   arc(77, 400, 88, 88,((minuteAngle/2))%180, (-1*(minuteAngle/2))%180, CHORD);
 
    //HOURS
    strokeWeight(8);
@@ -44,9 +59,11 @@ function setup() {
    arc(0, 0, 260, 260, 0, hourAngle);
    fill(150,255,100);
    strokeWeight(2);
-   arc(-100, 500, 88, 88, -1*(180 + (minuteAngle/2)), (180 + (minuteAngle/2)), CHORD);
-   arc(100, 500, 88, 88,((minuteAngle/2))%180, (-1*(minuteAngle/2))%180, CHORD);
+   arc(-77, 300, 88, 88, -1*(180 + (hourAngle/2)), (180 + (hourAngle/2)), CHORD);
+   arc(77, 300, 88, 88,((hourAngle/2))%180, (-1*(hourAngle/2))%180, CHORD);
      
+     
+   //Clock Hands
    push();
    rotate(secondAngle);
    stroke(255, 100, 150);
@@ -66,12 +83,17 @@ function setup() {
    pop();
 
    fill(255,255,255);
+   strokeWeight(8);
    point(0, 0);
+   stroke(255,255,255);
+   noFill();
+   ellipse(-77, 300,100,100);
+   ellipse(-77, 500,100,100);
+   ellipse(-77, 400,100,100);
+   ellipse(77, 300,100,100);
+   ellipse(77, 500,100,100);
+   ellipse(77, 400,100,100);
 
-
-   //  fill(255);
-   //  noStroke();
-   //  text(hr + ':' + mn + ':' + sc, 10, 200);
 
 
  }
